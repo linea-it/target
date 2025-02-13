52e05cd62592288506c09ceafcaf77be2a384c0f
jandsonrj <jandson1512@gmail.com>
2025-02-13 17:35:07 +0000

    implementation of the clear form button on the new catalog registration screen

--- frontend/src/containers/RegisterCatalog/Toolbar.js
+++ frontend/src/containers/RegisterCatalog/Toolbar.js
@@ -7,7 +7,9 @@ export default function RegisterCatalogToolbar(props) {
   return (
     <Toolbar>
       {/* {!props.onPrev && <Button variant="contained" color="secondary" disabled>Prev</Button>} */}
-      {props.onPrev && <Button variant="contained" color="secondary" onClick={() => props.onPrev()} disabled={!props.onPrev} >Prev</Button>}
+      {props.onPrev && <Button variant="contained" color="secondary" onClick={() => props.onPrev()} disabled={!props.onPrev}>Prev</Button>}
+
+      {props.onClear && (<Button variant="contained" color="secondary" onClick={() => props.onClear()} sx={{ mr: 1 }}>Clear Form</Button>)}
       <Box sx={{ flexGrow: 1 }} />
 
       {!props.onSubmit && <Button variant="contained" color="primary" onClick={() => props.onNext()} disabled={!props.onNext}>Next</Button>}
