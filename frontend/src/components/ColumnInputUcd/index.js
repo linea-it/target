3b710c3244a0f66e1cae2ffdc0772fd54b485bb5
glaubervila <glauber.vila.verde@gmail.com>
2025-07-18 11:49:54 -0300

    Refactors ColumnInputUcd component to simplify UI
    Updates the rendering logic to conditionally display only labels without ucd.

--- frontend/src/components/ColumnInputUcd/index.js
+++ frontend/src/components/ColumnInputUcd/index.js
@@ -1,11 +1,9 @@
-import EditIcon from '@mui/icons-material/Edit'
 import {
   FormControl,
   MenuItem,
   Stack,
   TextField
 } from '@mui/material'
-import IconButton from '@mui/material/IconButton'
 import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 
@@ -18,9 +16,6 @@ export default function ColumnInputUcd({ column, options, onChange }) {
     setValue(e.target.value)
     onChange(column, e.target.value)
   }
-  const handleChangeType = () => {
-    type === 'select' ? setType('text') : setType('select')
-  }
   return (
     <FormControl>
       <Stack direction="row" spacing={2}>
@@ -29,17 +24,13 @@ export default function ColumnInputUcd({ column, options, onChange }) {
           <TextField select value={value} onChange={handleChange}>
             {options.map(ucd => (
               <MenuItem key={`${column.name}_${ucd.value}`} value={ucd.value}>
-                {ucd?.label && (`${ucd.label} (${ucd.value})`) || ucd.value}
+                {ucd?.label ? ucd.label : ucd.value}
+                {/* Uncomment if you want to show the Label and UCD as well */}
+                {/* {ucd?.label && (`${ucd.label} (${ucd.value})`) || ucd.value} */}
               </MenuItem>
             ))}
           </TextField>
         )}
-        {/* {type === 'text' && (
-          <TextField value={value} onChange={handleChange}></TextField>
-        )}
-        <IconButton onClick={handleChangeType}>
-          <EditIcon />
-        </IconButton> */}
       </Stack>
     </FormControl>
   )
