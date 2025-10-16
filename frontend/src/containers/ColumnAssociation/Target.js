33ef77d89b43a6f197b16178af8dddfa5388e5c2
glaubervila <glauber.vila.verde@gmail.com>
2025-10-16 21:38:32 +0000

    Add UcdColumnMapper component and refactor ColumnAssociation structure

---
+++ frontend/src/containers/ColumnAssociation/Target.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from "@mui/material/Typography";
+import { ucds } from "@/data/ucds";
+
+import UcdColumnMapper from "@/components/UcdColumnMapper";
+
+
+export default function ColumnAssociationTarget({ catalog, onValidationChange }) {
+
+
+  return (
+    <Box>
+      <Stack
+        direction="row"
+        spacing={4}
+        sx={{
+          justifyContent: "center",
+          alignItems: "stretch",
+        }}
+      >
+        <Box>
+          <Typography variant="body1" gutterBottom sx={{ mb: 4 }}>
+            Please associate the column names of table <b>{catalog.table}</b> with those expected by the tool.
+          </Typography>
+
+          <UcdColumnMapper
+            catalog_id={catalog.id}
+            catalog_type="target"
+            ucds={ucds}
+            onValidationChange={onValidationChange} />
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
--- frontend/src/containers/RegisterCatalog/ColumnAssociation.js
+++ frontend/src/containers/RegisterCatalog/ColumnAssociation.js
@@ -1,18 +1,15 @@
 import React, { } from "react";
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Typography from "@mui/material/Typography";
 import RegisterCatalogToolbar from "./Toolbar";
 import { useRegisterCatalog } from "@/contexts/RegisterCatalogContext";
-import { mandatoryUcds, membersMandatoryUcds } from "@/data/ucds";
-import ColumnAssociation from "@/components/ColumnAssociation";
+
+import ColumnAssociationContainer from "@/containers/ColumnAssociation";
 
 export default function RegisterCatalogColumnAssociation() {
 
   const { setActiveStep, catalog } = useRegisterCatalog();
 
   const [isValid, setIsValid] = React.useState(false);
-  const [isRelatedValid, setIsRelatedValid] = React.useState(false);
 
   const handleNext = () => {
     setActiveStep(prevActiveStep => prevActiveStep + 1);
@@ -22,68 +19,11 @@ export default function RegisterCatalogColumnAssociation() {
     setActiveStep(prevActiveStep => prevActiveStep - 1);
   }
 
-
-  const typeTarget = () => {
-    return (
-      <>
-        <Typography variant="body1" gutterBottom>
-          Please associate the column names of table <b>{catalog.table}</b> with those expected by the tool.
-        </Typography>
-
-        <ColumnAssociation catalog_id={catalog.id} requiredUcds={mandatoryUcds} onValidationChange={setIsValid} />
-
-        <RegisterCatalogToolbar onNext={isValid ? handleNext : undefined} onPrev={handlePrev} />
-      </>
-    )
-  }
-
-  const typeCluster = () => {
-
-    return (
-      <>
-        <Stack
-          direction="row"
-          spacing={4}
-          sx={{
-            justifyContent: "center",
-            alignItems: "stretch",
-          }}
-        >
-          <Box>
-            <Typography variant="body1" gutterBottom>
-              Please associate the column names of table <b>{catalog.table}</b> with those expected by the tool.
-            </Typography>
-
-            <ColumnAssociation catalog_id={catalog.id} requiredUcds={mandatoryUcds} onValidationChange={setIsValid} />
-          </Box>
-          <Box>
-            <Typography variant="body1" gutterBottom>
-              Please associate the column names of table <b>{catalog.related_table_name.split('.')[1]}</b> with those expected by the tool.
-            </Typography>
-
-            <ColumnAssociation catalog_id={catalog.related_table} requiredUcds={membersMandatoryUcds} onValidationChange={setIsRelatedValid} />
-          </Box>
-        </Stack>
-        <RegisterCatalogToolbar onNext={(isValid && isRelatedValid) ? handleNext : undefined} onPrev={handlePrev} />
-      </>
-    )
-  }
-
   return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { mb: 2 },
-        '& .MuiTextField-root': { width: '30ch' }
-      }}
-      noValidate
-      autoComplete="off"
-    >
+    <Box>
+      <ColumnAssociationContainer catalog={catalog} onValidationChange={setIsValid} direction="row" />
 
-      {catalog.catalog_type === 'target' && (typeTarget())}
-
-      {catalog.catalog_type === 'cluster' && (typeCluster())}
-
+      <RegisterCatalogToolbar onNext={(isValid) ? handleNext : undefined} onPrev={handlePrev} />
     </Box>
   );
 }
