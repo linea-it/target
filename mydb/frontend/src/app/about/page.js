1053558bf6ec6bfe8e854090670e4f2ed34d634b
glaubervila <glauber.vila.verde@gmail.com>
2025-10-27 11:04:20 -0300

    Add About page. Closed #131

---
+++ mydb/frontend/src/app/about/page.js
@@ -0,0 +1,106 @@
+import Box from "@mui/material/Box";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+export default function About() {
+  return (
+    <Container>
+      <Box
+        component="main"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "calc(100vh - 64px - 496px)",
+          padding: 2,
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Breadcrumbs aria-label="breadcrumb" sx={{ mt: 2 }}>
+              <Link color="inherit" href="/">
+                Home
+              </Link>
+              <Typography color="textPrimary">About</Typography>
+            </Breadcrumbs>
+            <Typography variant="h6" sx={{ mb: 2 }}>
+              About
+            </Typography>
+            <Typography variant="body1" component="span">
+              TODO: MyDB about text
+            </Typography>
+            <Divider sx={{ mt: 4, mb: 4 }} />
+            <Typography variant="h6" sx={{ mb: 2 }}>
+              About Us
+            </Typography>
+            <Typography variant="body1">
+              LIneA is a multi-user laboratory operated by a non-profit
+              organization with financial support from the Brazilian Ministry of
+              Science, Technology, and Innovation. Our mission is to support the
+              Brazilian astronomical community with computing infrastructure and
+              big data analysis expertise to provide technical conditions for
+              participation in large astronomical surveys, such as SDSS, DES,
+              and LSST.
+            </Typography>
+            <Typography variant="body1">
+              <br />
+              If you are a Portuguese speaker, please find more about LIneA on
+              our{" "}
+              <Link
+                href="https://youtu.be/jC-k85tfd0Y"
+                target="_blank"
+                rel="noreferrer"
+              >
+                YouTube Channel
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="http://linea.org.br/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Website
+              </Link>
+              .
+            </Typography>
+            <Divider sx={{ mt: 4, mb: 4 }} />
+            <Typography variant="h6" sx={{ mb: 2 }}>
+              Acknowledgements
+            </Typography>
+            <Typography variant="body1" component="blockquote">
+              This application has made use of &quot;Aladin sky atlas&quot;
+              developed at CDS, Strasbourg Observatory, France →{" "}
+              <Link
+                href="https://ui.adsabs.harvard.edu/abs/2000A%26AS..143...33B/abstract"
+                target="_blank"
+                rel="noreferrer"
+              >
+                2000A&AS..143...33B
+              </Link>{" "}
+              (Aladin Desktop),{" "}
+              <Link
+                href="https://ui.adsabs.harvard.edu/abs/2014ASPC..485..277B/abstract"
+                target="_blank"
+                rel="noreferrer"
+              >
+                2014ASPC..485..277B
+              </Link>{" "}
+              (Aladin Lite v2), and{" "}
+              <Link
+                href="https://ui.adsabs.harvard.edu/abs/2022ASPC..532....7B/abstract"
+                target="_blank"
+                rel="noreferrer"
+              >
+                2022ASPC..532....7B
+              </Link>{" "}
+              (Aladin Lite v3).
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      {/* <Footer /> */}
+    </Container>
+  );
+}
--- mydb/frontend/src/contexts/AuthContext.js
+++ mydb/frontend/src/contexts/AuthContext.js
@@ -51,7 +51,7 @@ export function AuthProvider({ children }) {
         })
         .catch((err) => console.error("Erro ao recuperar settings", err));
     }
-  }, []);
+  });
 
   function logout() {
     const csrftoken = getCookie("target.csrftoken");
